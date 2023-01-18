8853cb5f8d93591936bf899863b79f963b964bff
alexurtti <alexurtti@hotmail.com>
2023-01-18 16:08:16 +0100

    identity db

---
+++ JaffChat/Server/Identity/Controller/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JaffChat.Server.Identity.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        public IdentityController()
+        {
+
+        }
+        public async Task<IActionResult> Register()
+        {
+
+
+            return null;
+        }
+    }
+}
