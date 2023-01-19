58da87feb52f8e2672475993932b5296060f54c0
Chiltrin <90396309+Chiltrin@users.noreply.github.com>
2023-01-19 13:10:21 +0100

    Made a model with AddUser so that frontend could launch

---
+++ jaffChat/Client/Models/Account/AddUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JaffChat.Client.Models.Account
+{
+    public class AddUser
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "The Password field must be a minimum of 6 characters")]
+        public string Password { get; set; }
+    }
+}
